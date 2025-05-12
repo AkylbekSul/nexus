68288b452f6d06962b27cc5069600473573fd994
a.sultanov <a.sultanov@smartpay.company>
2025-05-12 14:21:39 +0600

    Add frame ancestors header configuration to enhance security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_frame_ancestors_heade
 
-  protected 
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
+  end
+
+  def set_frame_ancestors_header
+    response.headers['Content-Security-Policy'] = "frame-ancestors https://iframetester.com/"
   end
 end
