e0b9d5efea178a487238b980c65d0642ad24bcd5
a.sultanov <a.sultanov@smartpay.company>
2025-05-12 14:13:51 +0600

    Add session store configuration to allow cross-site cookies

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module Nexus
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    # Allow cross-site cookies for iframe embedding
+    config.action_dispatch.cookies_same_site_protection = :none
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
